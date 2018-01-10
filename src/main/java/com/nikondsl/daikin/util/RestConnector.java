cb9b564991fc5c55841ccf862e31cde267dd6ec9
inikonov <Pered@$t2>
2018-01-10 17:33:08 +0200

    replaced restassured in production with httpclient

--- src/main/java/com/nikondsl/daikin/util/RestConnector.java
+++ src/main/java/com/nikondsl/daikin/util/RestConnector.java
@@ -4,17 +4,23 @@ import com.nikondsl.daikin.DaikinBase;
 import org.apache.http.Header;
 import org.apache.http.HeaderIterator;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.ProtocolVersion;
 import org.apache.http.RequestLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 import sun.misc.IOUtils;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +40,7 @@ public class RestConnector {
         try {
             HttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(createRequestConfig()).build();
             HttpResponse response = httpClient.execute(new HttpGet( daikin.getHost()+":"+daikin.getPort() + path));
-            if (verboseOutput) System.err.println("response=" + response.getEntity().getContent() + "\n");
+            if (verboseOutput) System.err.println("response=" + EntityUtils.toString(response.getEntity()));
             return Collections.singletonList(new String(IOUtils.readFully(response.getEntity().getContent(), (int)response.getEntity().getContentLength(), true)));
         } catch (Exception ex) {
             return null;
@@ -42,15 +48,23 @@ public class RestConnector {
     }
 
     public static String submitPost(DaikinBase daikin, String path, String post, Map<String, String> parameters, boolean verboseOutput) {
-//        try {
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(createRequestConfig()).build();
+            HttpPost httpPost = new HttpPost(daikin.getHost() + ":" + daikin.getPort() + path);
+            List<NameValuePair> params = new ArrayList<>();
+            parameters.entrySet().stream().forEach(entry -> {
+                params.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            });
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = httpClient.execute(httpPost);
 //            RequestSpecification spec = new RequestSpecBuilder().setBaseUri(daikin.getHost()).setPort(daikin.getPort()).build();
-//            if (verboseOutput) System.err.println("request=" + post);
+            if (verboseOutput) System.err.println("request=" + post);
 //            ResponseOptions response = given().spec(spec).parameters(parameters).when().post(path);
-//            if (verboseOutput) System.err.println("response=" + response.body().asString() + "\n");
+            if (verboseOutput) System.err.println("response=" + EntityUtils.toString(response.getEntity()));
+            return EntityUtils.toString(response.getEntity());
 //            return response.body().asString();
-//        } catch (Exception ex) {
-//            return null;
-//        }
-        return null;
+        } catch (Exception ex) {
+            return null;
+        }
     }
 }
