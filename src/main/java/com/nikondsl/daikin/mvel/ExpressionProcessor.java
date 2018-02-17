daac0e8cef130633a990574b94284ab4662bdbbe
igor.nikonoff <igor.nikonoff@gmail.com>
2018-02-17 13:50:38 +0200

    added domj4 & first part of parsing
    Took 1 hour 11 minutes

--- src/main/java/com/nikondsl/daikin/mvel/ExpressionProcessor.java
+++ src/main/java/com/nikondsl/daikin/mvel/ExpressionProcessor.java
@@ -1,16 +1,28 @@
 package com.nikondsl.daikin.mvel;
 
-import java.io.IOException;
-import java.util.*;
-import org.mvel2.*;
-import java.io.Serializable;
 import com.nikondsl.daikin.DaikinBase;
-import lombok.*;
-import com.nikondsl.daikin.wireless.*;
-import com.nikondsl.daikin.*;
+import com.nikondsl.daikin.DaikinFactory;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+import org.dom4j.tree.DefaultElement;
+import org.mvel2.MVEL;
+
+import java.io.File;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExpressionProcessor {
 	
+	@ToString
 	private static class Rule {
 		@Getter
 		@Setter
@@ -19,11 +31,21 @@ public class ExpressionProcessor {
 		@Setter
 		private DaikinBase action;
 		
+		@Getter
+		@Setter
+		private String nameOfRule;
+		
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		
+		loadFromConfig();
+		
+		if (true) return;
+//		Rule rule =new Rule();
+//		rule.setExpression("OuterTemperature < -1 && Power");
 		Rule rule =new Rule();
-		rule.setExpression("OuterTemperature < -1 && Power");
+		rule.setExpression("InnerTemperature <= 20 && !Power");
 		DaikinBase action = DaikinFactory.createWirelessDaikin("http://192.168.1.215", 80);
 		action.readDaikinState();
 		
@@ -33,7 +55,7 @@ public class ExpressionProcessor {
 		vars.put("InnerTemperature", action.getInsideTemperature());
 		vars.put("Power", action.isOn());
 		
-		action.setOn(false);
+		action.setOn(true);
 		rule.setAction(action);
 		
 //		String expr="4 > OuterTemperature && !Power";
@@ -43,5 +65,32 @@ public class ExpressionProcessor {
 		if (ret) rule.getAction().updateDaikinState();
 		
 		System.err.println("ret="+ret);
+	}
+	
+	public static Document parse(File rulesFile) throws DocumentException {
+		SAXReader reader = new SAXReader();
+		Document document = reader.read(rulesFile);
+		return document;
+	}
+	
+	private static List<Rule> loadFromConfig() throws DocumentException {
+		Path path = Paths.get("rules.xml").toAbsolutePath();
+		Document rules = parse(path.toFile());
+		List<Node> list = rules.selectNodes("//rule");
+	
+		for(Node node : list) {
+			String name = ((DefaultElement) node).element("name").getText();
+			String expression = ((DefaultElement) node).element("expression").getText();
+			Node actionNode = node.selectSingleNode("action");
+			String power = ((DefaultElement) actionNode).element("power").getText();
+			Rule rule = new Rule();
+			rule.setNameOfRule(name);
+			rule.setExpression(expression);
+			DaikinBase action = DaikinFactory.createWirelessDaikin("http:192.168.1.215", 80);
+			action.setOn("on".equalsIgnoreCase(power));
+			rule.setAction(action);
+			System.err.println("=== "+ rule);
+		}
+		return null;
 	}
 }
